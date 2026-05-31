6700387d04aa206beb8a198f9d55919e7a652b1d
DvestiVosemnadtsat <dba00839@gmail.com>
2026-05-31 21:03:36 +0300

    fhg

--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+899c936431f7d466c38eff9cf24fdd5991ad45a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7b10713530f25ad8b1d1b99926bdf1565b3ce22")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
